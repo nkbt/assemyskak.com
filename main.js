24877a2f563ea099341019905a96680de663169a
Nik Butenko <nik@butenko.me>
2019-11-18 17:47:58 +0800

    Fill Illustration items

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const rgb2hex = (rgb) => {
+  // Choose correct separator
+  const sep = rgb.indexOf(",") > -1 ? "," : " ";
+  // Turn "rgb(r,g,b)" into [r,g,b]
+  rgb = rgb.substr(4).split(")")[0].split(sep);
+
+  let r = (+rgb[0]).toString(16),
+    g = (+rgb[1]).toString(16),
+    b = (+rgb[2]).toString(16);
+
+  if (r.length === 1) {
+    r = "0" + r;
+  }
+  if (g.length === 1) {
+    g = "0" + g;
+  }
+  if (b.length === 1) {
+    b = "0" + b;
+  }
+
+  return "#" + r + g + b;
+};
+
+
 const lookup = (el, selector) => {
   let _el = el;
   while (_el && !_el.matches(selector)) {
@@ -47,7 +71,8 @@ subscribe('click', '[data-work] > a', (event, el) => {
     popup.style.top = `${y}px`;
     popup.style.left = `${x}px`;
     const fill = el.querySelector('.frame .fill');
-    popup.style.backgroundColor = getComputedStyle(fill).fill;
+    const color = getComputedStyle(fill).fill;
+    popup.style.backgroundColor = `${rgb2hex(color)}aa`;
     requestAnimationFrame(() => {
       popup.classList.add('animated');
       popup.style.top = `0px`;
